b545cd3887c656b50daa0ae6033313d92f9f3062
IaagoTavares <99209029+IaagoTavares@users.noreply.github.com>
2023-03-06 15:05:18 -0300

    adicionando comentários

--- src/main/java/br/edu/inteli/cc/m5/grupo/Points.java
+++ src/main/java/br/edu/inteli/cc/m5/grupo/Points.java
@@ -7,23 +7,23 @@ import br.edu.inteli.cc.m5.dted.DtedDatabaseHandler;
 public class Points {
   // Open DTed DataBase
 
+  // The purpose of this method is to open a DTED (Digital Terrain Elevation Data) database located at the given path.
   public static DtedDatabaseHandler openDtedDB(String path) {
+  // To do this, it creates a new instance of the "DtedDatabaseHandler" class and initializes it.
     DtedDatabaseHandler dtedDB = new DtedDatabaseHandler();
     dtedDB.InitializeFromResources(path);
 
     return dtedDB;
   }
 
-
-
-
+  // method that will receive the coordinates of the points and return the height of the point
   public static double[] getHeight(DtedDatabaseHandler dbDTED, double longitude, double latitude){
     Optional<Integer> height = dbDTED.QueryLatLonElevation(longitude, latitude);
     double[] coordinates = { longitude, latitude, (double) height.get() };
     return coordinates;
   }
 
-  
+  // method that will receive the coordinate for receive the points of the areas around the point
   public static double[][] getCoord(DtedDatabaseHandler dbDTED, double lonInitial, double latInitial, int row, int col, double lonStep, double latStep) {
     double[][] coordData = new double[row * col][3]; 
   
@@ -33,11 +33,14 @@ public class Points {
   
     for (int i = 0; i < row; i++) { 
       for (int j = 0; j < col; j++) { 
+        // here we put the initial coordinates and the file of dted
         coordData[count] = getHeight(dbDTED, lon, lat);
+        // traverse the longitude (column) per row 
         lon += lonStep;
         count++;
       }
-      lat -= latStep; 
+      //later we traverse the latitude (line)
+      lat -= latStep;
       lon = lonInitial;
     }
   
@@ -45,8 +48,10 @@ public class Points {
   }
 
 public static void main(String[] args) {
+  // Here we open the DTED database located at the path "dted/rio"
   DtedDatabaseHandler dbRio = openDtedDB("dted/rio");
 
+  // Here we define the initial coordinates, the number of rows and columns, and the step between the points
   int rows = 5, cols = 4;
   double lonInitial = -43.4082;
   double latInitial = -22.1780;
