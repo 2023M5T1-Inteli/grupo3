24478818b5b5aed76a7c937c949747732335c90c
Felipe Sampaio <felipe.silva@sou.inteli.edu.br>
2023-03-09 17:07:46 -0300

    Intregração node com java

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import graphRoutes from './Routes/graphRoutes.js';
-import cors from 'cors'
+import express from "express";
+import graphRoutes from "./Routes/graphRoutes.js";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/graph', graphRoutes);
+app.use("/graph", graphRoutes);
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
+app.listen(4000, () => {
+  console.log("listening on port 4000");
 });
