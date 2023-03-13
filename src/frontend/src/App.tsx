d8ceb24f741b52b38643ee0c2c2848020f6db3e0
gabriosilva <gabrio.lina@hotmail.com>
2023-03-12 22:09:46 -0300

    docs(frontend): add comments to code

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,52 +3,52 @@ import './App.css';
 import './components/Filter.css';
 import Filter from './components/Filter';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import "./App.css";
-import "./components/Filter.css";
 import { Container } from "@mui/system";
 import PathInputBox from "./components/PathInputBox";
 import Header from "./components/Header";
-import { Box, TextField } from '@mui/material';
+import { Box } from '@mui/material';
 import Map from './components/Map';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
-
 function App() {
+  // Define a state variable called `showFilter` and its updater function `setShowFilter` using the `useState` hook, initialized to `false`
   const [showFilter, setShowFilter] = useState(false);
 
-  const toogleFilter = () => {
+  // Define a function called `toggleFilter` that toggles the value of `showFilter` between `true` and `false`
+  const toggleFilter = () => {
     setShowFilter(!showFilter);
   };
 
+  // Render the main JSX:
   return (
     <div className="App">
+      {/* Use a `Container` component from Material-UI to set the maximum width of the app to full screen */}
       <Container maxWidth={false}>
+        {/* Render a `Header` component */}
         <Header />
+        {/* Render a `PathInputBox` component */}
         <PathInputBox />
       </Container>
-      <button
-        className="App-link"
-        onClick={toogleFilter}
-      >
-
+      {/* Render a button that toggles the filter when clicked */}
+      <button className="App-link" onClick={toggleFilter}>
         <FilterAltIcon />
       </button>
-      <Filter show={showFilter} toggleFilter={toogleFilter} />
-      
-
+      {/* Render a `Filter` component with props `show` and `toggleFilter` */}
+      <Filter show={showFilter} toggleFilter={toggleFilter} />
+      {/* Render a `Grid2` component from Material-UI to center the content */}
       <Grid2 container justifyContent={"center"}>
-        <Box width={1250} height={500} >
-        
+        <Box width={1250} height={500}>
           <Box component="main" sx={{ width: "100%", height: "100%" }}>
-          
+            {/* Render a `Map` component */}
             <Map />
           </Box>
-
         </Box>
+        {/* Render an SVG element with class `graph` */}
         <svg className='graph' width="1250" height="500"></svg>
       </Grid2>
     </div>
   );
 }
 
+// Export the `App` component as the default export of this module
 export default App;
