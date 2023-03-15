fa14e220337aea6735a96eb66971f140df24342b
Renato Machado <renato.machado@sou.inteli.edu.br>
2023-03-15 13:49:00 -0300

    Variaveis de desenvolvimento

--- src/backend/src/neo4j/neo4j.js
+++ src/backend/src/neo4j/neo4j.js
@@ -1,5 +1,9 @@
 import neo4j from "neo4j-driver";
+import dotenv from "dotenv";
 
-const driver = neo4j.driver("<URI>", neo4j.auth.basic("<USERNAME>", "<PASSWORD>"))
+dotenv.config({ path: '../.env' });
+
+// const driver = neo4j.driver("neo4j+s://41f6b34f.databases.neo4j.io", neo4j.auth.basic("neo4j", "9Mk9OO68J2Xw1z-GaVY2XcPdIa-y4gwwcIqdKXdGYWE"))
+const driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD))
 
 export default driver;
