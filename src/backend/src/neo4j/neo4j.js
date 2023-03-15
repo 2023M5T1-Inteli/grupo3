d3e0d25544971130ca474f810fcab83c8c50c717
Renato Machado <renato.machado@sou.inteli.edu.br>
2023-03-15 13:56:37 -0300

    GraphService fix

--- src/backend/src/neo4j/neo4j.js
+++ src/backend/src/neo4j/neo4j.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: '../.env' });
 
-// const driver = neo4j.driver("neo4j+s://41f6b34f.databases.neo4j.io", neo4j.auth.basic("neo4j", "9Mk9OO68J2Xw1z-GaVY2XcPdIa-y4gwwcIqdKXdGYWE"))
 const driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD))
 
 export default driver;
