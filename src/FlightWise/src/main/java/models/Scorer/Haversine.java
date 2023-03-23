329efb071a8535c4cd9c1aa371d27c1eecd16f77
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2023-03-23 18:16:30 -0300

    feature: pontos de exclusão

--- src/FlightWise/src/main/java/models/Scorer/Haversine.java
+++ src/FlightWise/src/main/java/models/Scorer/Haversine.java
@@ -2,6 +2,8 @@ package models.Scorer;
 
 import models.vertex.IVertex;
 
+import java.awt.geom.Point2D;
+
 
 /*
 * The haversine formula determines the great-circle distance between two points on a sphere given
@@ -17,6 +19,27 @@ public class Haversine implements IScorer {
 
         double toLat = to.getPosition().getX();
         double toLong = to.getPosition().getY();
+
+        double dLat = Math.toRadians(toLat - fromLat);
+        double dLon = Math.toRadians(toLong - fromLong);
+        double lat1 = Math.toRadians(fromLat);
+        double lat2 = Math.toRadians(toLat);
+
+        double a = Math.pow(Math.sin(dLat / 2),2)
+                + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return R * c;
+    }
+
+
+    public double computeDistanceByPoint2D(Point2D point1, Point2D point2) {
+        double R = 6372.8; // Earth's Radius, in kilometers
+
+        double fromLat = point1.getX();
+        double fromLong = point1.getY();
+
+        double toLat = point2.getX();
+        double toLong = point2.getY();
 
         double dLat = Math.toRadians(toLat - fromLat);
         double dLon = Math.toRadians(toLong - fromLong);
