d0bfa3746779d41112ee0c5795d7bb9bec41a4dd
Renato Machado <renato.machado@sou.inteli.edu.br>
2023-04-04 17:19:26 -0300

    Change toString method of coordinates

--- src/main/java/utils/neo4j/Neo4JDatabaseHandler.java
+++ src/main/java/utils/neo4j/Neo4JDatabaseHandler.java
@@ -58,10 +58,16 @@ public class Neo4JDatabaseHandler {
 
     public void createRouteNode(ArrayList<CoordinateVertex> coordinates, Session session, String pathID) {
         String cypherQuery = "CREATE (r:Route {pathID: $path, coordinates: $coord})";
-//        JSONObject j = new JSONObject(coordinates.get(0).toString());
-        String coordinatesJson = coordinates.get(0).toString();
-        System.out.println(coordinatesJson);
-        System.out.println(coordinatesJson);
+
+        String coordinatesJson = "[";
+
+        for (int i = 0; i < coordinates.size(); i++){
+            coordinatesJson += coordinates.get(i).toString();
+            coordinatesJson += ",";
+        }
+
+        coordinatesJson += "]";
+
         try (Transaction tx = session.beginTransaction()) {
             tx.run(cypherQuery, Map.of("path", pathID, "coord", coordinatesJson));
 
