97a4a0acabd94170ca6ba255c7e6ef3c54e13c68
Renato Machado <renato.machado@sou.inteli.edu.br>
2023-04-05 11:26:32 -0300

    Removing useless code

--- src/main/java/controller/AStarController.java
+++ src/main/java/controller/AStarController.java
@@ -107,8 +107,6 @@ public class AStarController implements CommandLineRunner {
 
         // Send the local Graph structure to neo4J
         try (Session session = driver.session(SessionConfig.forDatabase("neo4j"))) {
-            neo4JDatabaseHandler.createFinalPathVertexes(route, session, pathID);
-            neo4JDatabaseHandler.createFinalPathEdges(route, session);
             neo4JDatabaseHandler.createRouteNode(route, session, pathPlanner.getRouteID());
         }
 
--- src/main/java/utils/neo4j/Neo4JDatabaseHandler.java
+++ src/main/java/utils/neo4j/Neo4JDatabaseHandler.java
@@ -4,57 +4,12 @@ import models.vertex.CoordinateVertex;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.Transaction;
 
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Map;
 
 public class Neo4JDatabaseHandler {
 
     public Neo4JDatabaseHandler() {}
-
-    // Inserts into the Neo4J database, the final optimal path as a new connection type
-    public void createFinalPathEdges(ArrayList<CoordinateVertex> coordinates, Session session){
-        String cypherQuery = "MATCH (n:NewCoordinate {index: $ind1})\n" +
-                "MATCH (c:NewCoordinate {index: $ind2})\n" +
-                "CREATE (n)-[r:Route]->(c)";
-        try (Transaction tx = session.beginTransaction()) {
-            for (int i = 0; i < coordinates.size() - 1; i++) {
-                CoordinateVertex coordinateVertex = coordinates.get(i);
-                CoordinateVertex nextVertex = coordinates.get(i+1);
-                tx.run(cypherQuery, Map.of("ind1", coordinateVertex.getIndex(), "ind2", nextVertex.getIndex()));
-            }
-            tx.commit();
-        }
-    }
-
-    // Updates the Neo4J database, changing the final path vertexes type
-    public void createFinalPathVertexes(ArrayList<CoordinateVertex> coordinates, Session session, String pathID){
-        String cypherQuery = "CREATE (c:NewCoordinate {index: $ind, lastNode: $last, latitude: $lat, longitude: $long, averageHeight: $avgHeight, pathID: $path})";
-
-        try (Transaction tx = session.beginTransaction()) {
-            for (int i = 0; i < coordinates.size(); i++) {
-                CoordinateVertex coordinateVertex = coordinates.get(i);
-                Point2D position = coordinateVertex.getPosition();
-
-                int lastNodeIndex = -1;
-
-                if (coordinateVertex.previousVertex != null){
-                    lastNodeIndex = coordinateVertex.previousVertex.getIndex();
-                }
-
-
-                int previousVertexIndex = -1;
-                if (coordinateVertex.previousVertex != null) {
-                    previousVertexIndex = coordinateVertex.previousVertex.getIndex();
-                }
-                tx.run(cypherQuery, Map.of("ind", coordinateVertex.getIndex(), "last", previousVertexIndex, "lat", position.getX(), "long", position.getY(), "avgHeight", coordinateVertex.averageHeight, "path", pathID));
-
-
-            }
-
-            tx.commit();
-        }
-    }
 
     public void createRouteNode(ArrayList<CoordinateVertex> coordinates, Session session, String pathID) {
         String cypherQuery = "CREATE (r:Route {pathID: $path, coordinates: $coord})";
