b8c19f56defbbeb3236de6943873cd25caa3cbf6
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2023-04-06 18:34:16 -0300

    docs: unit test correction

--- src/test/java/models/Graph/GraphTest.java
+++ src/test/java/models/Graph/GraphTest.java
@@ -1,30 +1,19 @@
 package models.Graph;
 
 import models.vertex.CoordinateVertex;
+import org.junit.jupiter.api.Test;
 
 import java.awt.geom.Point2D;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GraphTest {
-    /*
-     * "addVertex" is a void method, so it's hard to determine if it is performing as expected.
-     * To verify this, we can initialize a "Graph" and a "CoordinateVertex" object and compare the size of the
-     * ArrayList before and after calling "addVertex".
-     *
-     * By using this approach, we can test both the "addVertex" and "getVertexes" methods at the same time.
-     * */
-    CoordinateVertex vertexToTest = new CoordinateVertex((new Point2D.Double(30.4, 12.3)), 1000);
-
-    @org.junit.jupiter.api.Test
-    void addVertex() {
-        Graph graphToTest = new Graph(1,1);
-
-        assertEquals(0, graphToTest.getVertexes().size());
-        graphToTest.addVertex(vertexToTest, 0,0);
-        assertEquals(1, graphToTest.getVertexes().size());
+    @Test
+    public void testAddVertex() {
+        Graph graph = new Graph(2, 2);
+        CoordinateVertex vertex = new CoordinateVertex(new Point2D.Double(20.0, 21.2), 2);
+        graph.addVertex(vertex, 0, 0);
+        assertEquals(1, graph.totalVertices);
     }
-
-
 
 }
